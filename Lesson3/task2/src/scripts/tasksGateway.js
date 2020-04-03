126d6c27636e5e9da85d2be8a52c1ddf71070355
MaksShadows <Lykanenko1995@gmail.com>
2020-04-03 16:40:20 +0300

    task2

---
+++ Lesson3/task2/src/scripts/tasksGateway.js
@@ -0,0 +1,39 @@
+const baseUrl = 'https://crudcrud.com/api/a9ec75833dfe4bf1b5de1e0797328f48/tasks';
+
+const mapTasks = tasks =>
+    tasks.map(({ _id, ...rest }) => ({...rest, id: _id }));
+
+export const getTasksList = () => {
+    return fetch(baseUrl)
+        .then(response => response.json())
+        .then(tasks => mapTasks(tasks))
+};
+
+export const createTask = taskData => {
+    return fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(taskData)
+    })
+};
+
+export const updateTask = (taskId, updatedTaskData) => {
+    return fetch(`${baseUrl}/${taskId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(updatedTaskData),
+    });
+};
+
+export const deleteTask = (taskId) => {
+    return fetch(`${baseUrl}/${taskId}`, {
+        method: 'DELETE',
+
+    })
+};
+
+
